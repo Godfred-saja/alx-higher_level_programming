9b544b048cd6a386407b679dc769fc1b9260ab11
Godfred-saja <godfredakowuah2002@gmail.com>
2023-08-11 19:20:13 +0000

    asa

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
 
-nargss = len(sys.argv) - 1
-if nargss != 3:
-print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-sys.exit(1)
+    opps = ['+', '-', '*', '/']
 
-opp = sys.argv[2]
-if opp != '+' and opp != '-' and opp != '*' and opp != '/':
-print("Unknown operator. Available operators: +, -, * and /")
-sys.exit(1)
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
 
-from calculator_1 import add, sub, mul, div
-a = int(sys.argv[1])
-b = int(sys.argv[3])
+    a = int(argv[1])
+    operator = (argv[2])
+    b = int(argv[3])
 
-if opp == '+':
-print("{} + {} = {}".format(a, b, add(a, b)))
-elif opp == '-':
-print("{} - {} = {}".format(a, b, sub(a, b)))
-elif opp == '*':
-print("{} * {} = {}".format(a, b, mul(a, b)))
-else:
-print("{} / {} = {}".format(a, b, div(a, b)))
+    if operator not in opps:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+
+    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
