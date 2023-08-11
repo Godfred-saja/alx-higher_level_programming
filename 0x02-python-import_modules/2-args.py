c905b368a800ba8b3263b9fc6c8d4156e606f116
Godfred-saja <godfredakowuah2002@gmail.com>
2023-08-11 14:07:17 +0000

    lkj

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-f __name__ == "__main__":
-import sys
-i = len(sys.argv) - 1
+if __name__ == "__main__":
+    from sys import argv
 
-if i == 0:
-print("{} arguments.".format(i))
-elif i == 1:
-print("{} argument:".format(i))
+if len(argv) == 1:
+    print("{:d} arguments.".format(len(argv) - 1))
 else:
-print("{} arguments:".format(i))
+    if len(argv) == 2:
+        print("{:d} argument:".format(len(argv) - 1))
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
 
-if i >= 1:
-i = 0
-for arg in sys.argv:
-if i != 0:
-print("{}: {}".format(i, arg))
-i += 1
+    for i in range(1, len(argv)):
+        print("{:d}: {:s}".format(i, argv[i]))
