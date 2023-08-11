4e22f4c68f5ce9a044ad6a2ffcfcdfebe5f3532d
Godfred-saja <godfredakowuah2002@gmail.com>
2023-08-11 14:11:00 +0000

    asd

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
-result = 0
-for arg in sys.argv:
-if arg != sys.argv[0]:
-result += int(arg)
-print(result)
+    from sys import argv
+
+    result = 0
+    for i in range(len(argv) - 1):
+        result += int(argv[i + 1])
+    print("{:d}".format(result))
